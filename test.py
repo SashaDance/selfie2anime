8a95b5ddb218f35124e0b44d5f1564c8b4335054
SashaDance <sksslr@mail.ru>
2024-02-05 16:09:27 +0300

    feat(test): added some testcases to dataset test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from dataset import ImageDataset
 import torch
 
 
-def test_image_dataset(image_size: int = 128) -> None:
+def test_image_dataset(in_channels:int = 3, image_size: int = 128) -> None:
     female_tr = ImageDataset('trainA')
     anime_tr = ImageDataset('trainB')
     female_test = ImageDataset('testA')
@@ -14,10 +14,15 @@ def test_image_dataset(image_size: int = 128) -> None:
     assert len(female_test) == len(anime_test) == 100, \
         f'Lost some train data: only {len(female_test)} and {len(anime_test)} of 100'
 
-    print(female_tr[0].shape)
-    print(anime_tr[1].shape)
-    print(female_test[2].shape)
-    print(anime_test[3].shape)
+    img_shape = [in_channels, image_size, image_size]
+    assert list(female_tr[0].shape) == img_shape, \
+        f'Incorrect image shape: got {list(female_tr[0].shape)}, expected: {img_shape}'
+    assert anime_tr[0].shape == torch.Size([3, 128, 128]), \
+        f'Incorrect image shape: got {list(anime_tr[0].shape)}, expected: {img_shape}'
+    assert female_test[0].shape == torch.Size([3, 128, 128]), \
+        f'Incorrect image shape: got {list(female_test[0].shape)}, expected: {img_shape}'
+    assert anime_test[0].shape == torch.Size([3, 128, 128]), \
+        f'Incorrect image shape: got {list(anime_test[0].shape)}, expected: {img_shape}'
 
 
 def main() -> None:
