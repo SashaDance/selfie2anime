46b6769ab8b0a0cf0eed2206581d04a3634c774d
SashaDance <sksslr@mail.ru>
2024-02-06 11:55:39 +0300

    feat(cycle gan): implemented discriminator gradient descent step

--- cycle_gan.py
+++ cycle_gan.py
@@ -1,20 +1,101 @@
 from discriminator import Discriminator
 from generator import Generator
 
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+from torch.optim import Optimizer
 import torch.nn as nn
 import torch
 
+
 class CycleGAN:
+    """
+    CycleGAN architecture consists of two Generators and two Discriminators
+    """
     def __init__(self, device: torch.device,
                  in_channels: int = 3,
                  filters: int = 64,
                  num_resid: int = 6):
-        # generators
+        self.device = device
+
+        # generator from X to Y
         self.gen_XY = Generator(in_channels, filters, num_resid).to(device)
+        # generator from X to Y
         self.gen_YX = Generator(in_channels, filters, num_resid).to(device)
 
-        # discriminators
-        self.dis_XY = Discriminator(in_channels, filters)
-        self.dis_YX = Discriminator(in_channels, filters)
+        # discriminator of X images
+        self.dis_X = Discriminator(in_channels, filters)
+        # discriminator of Y images
+        self.dis_Y = Discriminator(in_channels, filters)
 
+    def __discriminator_step(self, optimizer: Optimizer,
+                             x_batch: torch.Tensor,
+                             y_batch: torch.Tensor) -> torch.Tensor:
+        optimizer.zero_grad()
 
+        # discriminator X
+
+        # predictions of discriminator on real x images
+        real_x_preds = self.dis_X(x_batch)
+        # calculating MSE loss for real x images
+        real_x_loss = torch.mean((real_x_preds - 1) ** 2)  # 1 is for the real images
+
+        # predictions of discriminator on fake x images
+        fake_x = self.gen_XY(x_batch)
+        fake_x_preds = self.dis_X(fake_x)
+        # calculating MSE loss for fake x images
+        fake_x_loss = torch.mean((fake_x_preds - 0) ** 2)  # 0 is for the real images
+
+        # updating weights for X discriminator
+        loss_x = real_x_loss + fake_x_loss
+        loss_x.backward()
+        optimizer.step()
+
+        # discriminator Y
+
+        # predictions of discriminator on real y images
+        real_y_preds = self.dis_Y(y_batch)
+        # calculating MSE loss for real y images
+        real_y_loss = torch.mean(
+            (real_y_preds - 1) ** 2)  # 1 is for the real images
+
+        # predictions of discriminator on fake y images
+        fake_y = self.gen_YX(y_batch)
+        fake_y_preds = self.dis_Y(fake_y)
+        # calculating MSE loss for fake y images
+        fake_y_loss = torch.mean(
+            (fake_y_preds - 0) ** 2)  # 0 is for the real images
+
+        # updating weights for Y discriminator
+        loss_y = real_y_loss + fake_y_loss
+        loss_y.backward()
+        optimizer.step()
+
+        return torch.Tensor()
+
+    def __generator_step(self, optimizer: Optimizer,
+                         x_batch: torch.Tensor,
+                         y_batch: torch.Tensor) -> torch.Tensor:
+        optimizer.zero_grad()
+
+        return torch.Tensor()
+
+    def train(self, epochs: int,
+              optimizers: dict[str, Optimizer],
+              train_loader_x: DataLoader,
+              train_loader_y: DataLoader) -> None:
+        losses = {}
+
+        for epoch in range(epochs):
+            for x_batch, y_batch in tqdm(zip(train_loader_y, train_loader_x)):
+                x_batch = x_batch.to(self.device)
+                y_batch = y_batch.to(self.device)
+
+                self.__discriminator_step(
+                    optimizers['discriminator'], x_batch, y_batch
+                )
+                self.__generator_step(
+                    optimizers['generator'], x_batch, y_batch
+                )
+
+                # TODO: save losses
