fd6cf8f2c2347e43b3de52add087d2c1c5661e49
SashaDance <sksslr@mail.ru>
2024-03-02 12:45:52 +0300

    feat(dataset): ImageBuffer init commit

--- dataset.py
+++ dataset.py
@@ -47,3 +47,44 @@ class ImageDataset(Dataset):
         image_transformed = self.transform(image)
 
         return image_transformed
+
+
+class ImageBuffer:
+    """
+    Buffer of generated images, which gives generator some time
+    to learn to fool the discriminator
+    """
+
+    def __init__(self, buffer_lim: int = 64, prob_threshold: float = 0.5):
+        """
+        :param buffer_lim: max size of buffer
+        :param prob_threshold: probability of putting new image to buffer
+        """
+        assert config.BATCH_SIZE <= buffer_lim, \
+            'Buffer limit should be greater than the batch size'
+        self.buffer_lim = buffer_lim
+        self.prob_threshold = prob_threshold
+        self.buffer: list[torch.Tensor] = []
+
+    def get_image(self, image_batch: torch.Tensor) -> torch.Tensor:
+        """
+        :param image_batch: current generated image barch
+        :return: image batch after sampling from buffer
+        """
+        # return current batch if there is less images in the buffer than the batch size
+        if len(self.buffer) < len(image_batch):
+            return image_batch
+
+        for image in image_batch:
+            # initializing of buffer
+            if len(self.buffer) < self.buffer_lim:
+                self.buffer.append(image)
+                images.append(image)
+            # updating the buffer
+            else:
+                prob = np.random.uniform()
+                ind = np.random.randint(0, self.buffer_lim)
+                if prob > self.prob_threshold:
+                    # add replace image in buffer
+
+
